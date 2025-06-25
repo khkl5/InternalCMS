507a9ce070e90d23dc9359cfb6701de0e42848d5
khkl5 <khkloookh@gmail.com>
2025-06-25 22:51:39 +0300

    10

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import update_task_status, task_list_view
 
 urlpatterns = [
-    path('update-status/<int:task_id>/', views.update_task_status, name='update_task_status'),
+    path('update-status/<int:task_id>/', update_task_status, name='update_task_status'),
+    path('list/', task_list_view, name='task_list'),
 ]
