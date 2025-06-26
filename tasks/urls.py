4c678fd0aa7132329530f0fb36f6dc179600a4df
khkl5 <khkloookh@gmail.com>
2025-06-26 14:10:18 +0300

    14

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from .views import update_task_status, task_list_view
 
 urlpatterns = [
     path('update-status/<int:task_id>/', update_task_status, name='update_task_status'),
-    path('list/', task_list_view, name='task_list'),
+    path('', task_list_view, name='task_list'),
+
 ]
